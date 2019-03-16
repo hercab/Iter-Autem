67d755b7c388a222feabcb558a0b9356d409ff6c
lqdominguez <lqdominguez@uniss.edu.cu>
2019-03-16 15:39:15 -0400

    Fixing tour guide

--- sites/all/modules/features/iterautem/ia_guest_reports/data/get_bussiness_type.sql
+++ sites/all/modules/features/iterautem/ia_guest_reports/data/get_bussiness_type.sql
@@ -1,5 +1,21 @@
 --ACCESS=access content
-SELECT DISTINCT bussiness_type_name as name, bussiness_type_tid FROM (
---INCLUDE=get_publicity
+SELECT DISTINCT bussiness_type_name as name, bussiness_type_tid
+FROM (
+
+SELECT field_data_field_bussiness_type.field_bussiness_type_tid AS bussiness_type_tid,
+taxonomy_term_data.name AS bussiness_type_name
+FROM node
+INNER JOIN field_data_field_sp_area ON (node.nid = field_data_field_sp_area.entity_id
+                                       AND field_data_field_sp_area.deleted = '0'
+                                       AND field_data_field_sp_area.delta = '0')
+INNER JOIN field_data_field_bussiness_type ON (node.nid = field_data_field_bussiness_type.entity_id
+                                             AND field_data_field_bussiness_type.deleted = '0'
+                                             AND field_data_field_bussiness_type.delta = '0')
+INNER JOIN taxonomy_term_data ON (field_data_field_bussiness_type.field_bussiness_type_tid = taxonomy_term_data.tid)
+
+WHERE node.status = '1'
+      AND node.type IN  ('publicity')
+	    AND field_data_field_sp_area.field_sp_area_tid = :area_id
+
 ) AS bussiness_types
 GROUP BY name
--- sites/all/modules/features/iterautem/ia_guest_reports/data/get_publicity.sql
+++ sites/all/modules/features/iterautem/ia_guest_reports/data/get_publicity.sql
@@ -2,34 +2,35 @@
 SELECT node.nid AS pub,  flagging.timestamp AS flagging_timestamp, field_data_field_bussiness_type.field_bussiness_type_tid AS bussiness_type_tid,
 taxonomy_term_data.name AS bussiness_type_name
 FROM node
-INNER JOIN flagging ON (flagging.fid = '2' AND flagging.uid = :current_user)
-INNER JOIN field_data_field_sp_area ON node.nid = field_data_field_sp_area.entity_id
-                                       AND (field_data_field_sp_area.entity_type = 'node'
-                                       AND field_data_field_sp_area.deleted = '0')
-LEFT JOIN field_data_field_bussiness_type ON node.nid = field_data_field_bussiness_type.entity_id
-                                             AND (field_data_field_bussiness_type.entity_type = 'node'
-                                             AND field_data_field_bussiness_type.deleted = '0')
-LEFT JOIN taxonomy_term_data ON field_data_field_bussiness_type.field_bussiness_type_tid = taxonomy_term_data.tid
+LEFT JOIN flagging ON (node.nid = flagging.entity_id
+			                  AND flagging.fid = '2'
+			                  AND flagging.uid = :current_user)
+INNER JOIN field_data_field_sp_area ON (node.nid = field_data_field_sp_area.entity_id
+                                       AND field_data_field_sp_area.deleted = '0'
+                                       AND field_data_field_sp_area.delta = '0')
+INNER JOIN field_data_field_bussiness_type ON (node.nid = field_data_field_bussiness_type.entity_id
+                                             AND field_data_field_bussiness_type.deleted = '0'
+                                             AND field_data_field_bussiness_type.delta = '0')
+INNER JOIN taxonomy_term_data ON (field_data_field_bussiness_type.field_bussiness_type_tid = taxonomy_term_data.tid)
 
---IF=:bussiness_type_tid
+WHERE node.status = '1'
+      AND node.type IN  ('publicity')
+	    AND field_data_field_sp_area.field_sp_area_tid = :area_id
+	    AND field_data_field_bussiness_type.field_bussiness_type_tid = :bussiness_type_tid
 
-WHERE (( (node.status = '1') AND (node.type IN  ('publicity'))
-         AND (field_data_field_sp_area.field_sp_area_tid = :area_id)
-         AND (flagging.timestamp <= :order_date)
-         AND (field_data_field_bussiness_type.field_bussiness_type_tid = :bussiness_type_tid)))
---ELSE
-
-WHERE (( (node.status = '1') AND (node.type IN  ('publicity'))
-         AND (field_data_field_sp_area.field_sp_area_tid = :area_id)
-         AND (flagging.timestamp <= :order_date)))
-
---END
-GROUP BY pub
 ORDER BY bussiness_type_tid ASC
 
-LIMIT 10 OFFSET 0
+--SWITCH=:bussiness_type_tid
+--CASE=40
+LIMIT 4 OFFSET 0
+--ELSE
+LIMIT 2 OFFSET 0
+--END
+
+
 
 --INFO
 type[bussiness_type_tid]=int
+type[limit]=int
 entity_type = node
 entity_id = pub
